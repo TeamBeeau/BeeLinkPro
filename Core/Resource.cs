7346b63b469c80111b4c7cf94a660ce941e871f7
Nguyen Chi Tu <Nguyen Chi Tu@CHITU-PC>
2023-10-05 08:56:54 +0700

    Add project files.

---
+++ Core/Resource.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public struct Resource
+    {
+        public static Color ColorBG(clBackground colorBG)
+        {
+          
+            switch (colorBG)
+            {
+                
+                case clBackground.Form:
+                    return Color.FromArgb(255, 205, 35);
+                case clBackground.UserCotrol:
+                    return Color.FromKnownColor(KnownColor.Gainsboro);
+                case clBackground.Panel:
+                    return Color.White;
+                case clBackground.Label:
+                    return Color.FromArgb(70, 70, 70);
+            }
+            return new Color();
+        }
+    }
+}
